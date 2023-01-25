f9d56ee407f51ee25b5bc0ad430ee1e447a09f96
mako719 <shoki.sm.sk@gmail.com>
2023-01-25 18:18:44 +0900

    CustomExceptionHandlerクラスのアノテーションの誤字を訂正

--- src/main/java/com/raisetech/customermanagement/CustomExceptionHandler.java
+++ src/main/java/com/raisetech/customermanagement/CustomExceptionHandler.java
@@ -3,6 +3,7 @@ package com.raisetech.customermanagement;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.ZonedDateTime;
@@ -11,7 +12,7 @@ import java.util.Map;
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = ResourceNotFoundException.class)
+    @ExceptionHandler(value = ResourceNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleNoResourceFound(
             ResourceNotFoundException e, HttpServletRequest request) {
 
