21b6d7f4af6901e9c063bc8a6c9c0c9b5573c247
mako719 <shoki.sm.sk@gmail.com>
2023-02-02 18:55:58 +0900

    結合テスト登録成功時

--- src/test/java/com/raisetech/customermanagement/integrationtest/UserRestApiIntegrationTest.java
+++ src/test/java/com/raisetech/customermanagement/integrationtest/UserRestApiIntegrationTest.java
@@ -2,23 +2,71 @@ package com.raisetech.customermanagement.integrationtest;
 
 import com.github.database.rider.core.api.dataset.DataSet;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.charset.StandardCharsets;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserRestApiIntegrationTest {
 
     @Autowired
-    MockMvc mockmvc;
+    MockMvc mockMvc;
 
     @Test
     @DataSet(value = "customers.yml")
     @Transactional
-    void 顧客情報が全件取得できること() throws Exception{
+    void 顧客情報が全件取得できること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/customers"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        JSONAssert.assertEquals(
+                "   {" +
+                        "       \"id\": 1," +
+                        "       \"name\": \"tanaka\"," +
+                        "       \"age\": 35," +
+                        "       \"site\": \"shoulder\"," +
+                        "       \"staff\": yamada" +
+                        "   }," +
+                        "   {" +
+                        "       \"id\": 2," +
+                        "       \"name\": \"suzuki\"," +
+                        "       \"age\": 28," +
+                        "       \"site\": \"neck\"," +
+                        "       \"staff\": yamamoto" +
+                        "   }" +
+                        "]", response, JSONCompareMode.STRICT);
+    }
 
+    @Test
+    @Transactional
+    void 顧客情報の登録成功し201レスポンスとLocationヘッダに登録したidとレスポンスとしてメッセージが返ること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.post("/customers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{" +
+                                "       \"id\": 1," +
+                                "       \"name\": \"tanaka\"," +
+                                "       \"age\": 35," +
+                                "       \"site\": \"shoulder\"," +
+                                "       \"staff\": yamada" +
+                                "   }")
+                )
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.header().string("Location", "http://localhost/customers/1"))
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("{" +
+                "       \"message\": \"顧客情報が登録されました。\"," +
+                "       \"id\": 1," +
+                "   }", response, JSONCompareMode.STRICT);
     }
 }
