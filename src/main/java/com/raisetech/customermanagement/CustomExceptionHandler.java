65123bd55cda45e8cc2586040ffcf57c857af0e8
mako719 <shoki.sm.sk@gmail.com>
2023-02-15 15:52:33 +0900

    不要なimport文を削除

--- src/main/java/com/raisetech/customermanagement/CustomExceptionHandler.java
+++ src/main/java/com/raisetech/customermanagement/CustomExceptionHandler.java
@@ -1,16 +1,12 @@
 package com.raisetech.customermanagement;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.ZonedDateTime;
 import java.util.HashMap;
